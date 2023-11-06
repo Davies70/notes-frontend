fe46d3599e98d86132f3010701bd7437493bea89
Ajayi Davies <ajayidavies7@gmail.com>
2023-11-06 10:51:43 +0100

    changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+/**
+ * Represents a notes app.
+ *
+ * @returns {JSX.Element} The rendered UI elements of the notes app.
+ */
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
+  // State hooks
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
+  // Effect hook
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes)
-      })
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes)
+    })
   }, [])
 
+  // Function to add a new note
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -27,63 +37,114 @@ const App = () => {
 
     noteService
       .create(noteObject)
-        .then(returnedNote => {
+      .then((returnedNote) => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
+      .catch((error) => console.log(error.response.data.error))
   }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
-   const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      noteService
-        .update(id, changedNote).then(returnedNote => {
-          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-        })
-        .catch(error => {
-          setErrorMessage(
-            `Note '${note.content}' was already removed from server`
-          )
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
-        })
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('User object', user)
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  )
 
   return (
     <div>
       <h1>Notes app</h1>
       <Notification message={errorMessage} />
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
-      </div> 
+      </div>
       <ul>
         <ul>
-          {notesToShow.map(note => 
+          {notesToShow.map((note) => (
             <Note
               key={note.id}
               note={note}
               toggleImportance={() => toggleImportanceOf(note.id)}
             />
-          )}
+          ))}
         </ul>
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+
       <Footer />
     </div>
   )
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,23 +1,42 @@
+/**
+ * A module that provides functions for performing CRUD operations on a collection of notes.
+ * @module noteService
+ */
+
 import axios from 'axios'
-const baseUrl = '/api/notes'
 
+/**
+ * Retrieves all notes from the API.
+ * @returns {Promise<Array>} A promise that resolves to an array of notes.
+ */
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get('/api/notes')
   return request.then(response => response.data)
 }
 
+/**
+ * Creates a new note.
+ * @param {Object} newObject - The new note object to be created.
+ * @returns {Promise<Object>} A promise that resolves to the created note object.
+ */
 const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
+  const request = axios.post('/api/notes', newObject)
   return request.then(response => response.data)
 }
 
+/**
+ * Updates an existing note.
+ * @param {string} id - The ID of the note to be updated.
+ * @param {Object} newObject - The updated note object.
+ * @returns {Promise<Object>} A promise that resolves to the updated note object.
+ */
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  const request = axios.put(`/api/notes/${id}`, newObject)
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll, 
-  create, 
-  update 
+export default {
+  getAll,
+  create,
+  update
 }
